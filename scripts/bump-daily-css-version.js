cc14a7415838c4c795a279d49443ea36a5f07ee0
QilyLean <DingHunter623@gmail.com>
2026-07-29 09:18:05 +0800

    优化今日简报：bump-daily-css-version.js

--- scripts/bump-daily-css-version.js
+++ scripts/bump-daily-css-version.js
@@ -14,7 +14,7 @@ const targets = [
 
 for (const file of targets) {
   const source = fs.readFileSync(file, 'utf8');
-  const next = source.replace(/\/qilylean\/daily-briefs\.css\?v=[^"']+/g, '/qilylean/daily-briefs.css?v=20260729-engineering-system-v9');
+  const next = source.replace(/\/qilylean\/daily-briefs\.css\?v=[^"']+/g, '/qilylean/daily-briefs.css?v=20260729-engineering-system-v10');
   if (next !== source) fs.writeFileSync(file, next);
 }
 
