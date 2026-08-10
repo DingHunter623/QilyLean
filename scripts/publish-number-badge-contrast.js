d6e2afbd0a88bcd0bf33b712fec3ec48de0bc338
QilyLean <DingHunter623@gmail.com>
2026-08-10 08:27:22 +0800

    chore: materialize floating dock order loader V14

--- scripts/publish-number-badge-contrast.js
+++ scripts/publish-number-badge-contrast.js
@@ -7,7 +7,7 @@ const path = require('path');
 const root = path.resolve(__dirname, '..');
 const NUMBER_VERSION = '20260805-number-badge-contrast-v1';
 const HOVER_VERSION = '20260810-sitewide-floating-dock-feedback-v13';
-const NAV_VERSION = '20260810-sitewide-floating-dock-loader-v13';
+const NAV_VERSION = '20260810-sitewide-floating-dock-loader-v14';
 const NUMBER_HREF = `/site-number-badge-contrast-v1.css?v=${NUMBER_VERSION}`;
 const HOVER_HREF = `/site-interactive-hover-contrast-v1.css?v=${HOVER_VERSION}`;
 const NAV_HREF = `/site-navigation.js?v=${NAV_VERSION}`;
