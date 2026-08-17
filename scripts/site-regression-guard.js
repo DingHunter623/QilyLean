cea63af678302809eecf213272f66d0886de4e1b
QilyLean <DingHunter623@gmail.com>
2026-08-17 16:06:07 +0800

    ci: bind friend-links layout guard into regression gate

--- scripts/site-regression-guard.js
+++ scripts/site-regression-guard.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-/* Compatibility entrypoint for QilyLean R2 regression guard v2. */
+/* Compatibility entrypoint for QilyLean R2 regression guard v2 + permanent friend-links/layout guard. */
 require('./site-regression-guard-v2.js');
+require('./site-friend-links-layout-guard.js');
